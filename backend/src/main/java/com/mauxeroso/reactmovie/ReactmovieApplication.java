035d1fbbd4d857ffacbd6d85b7be2d7507e444df
Caio <jilsorem@gmail.com>
2022-03-07 18:20:00 -0300

    Initial commit

---
+++ backend/src/main/java/com/mauxeroso/reactmovie/ReactmovieApplication.java
@@ -0,0 +1,13 @@
+package com.mauxeroso.reactmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mauxeroso/reactmovie/ReactmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mauxeroso.reactmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
