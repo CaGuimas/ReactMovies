42887d4f1ac770e485e4fe3df765f1a5984dad97
Caio <jilsorem@gmail.com>
2022-03-09 13:55:24 -0300

    form layout

--- frontend/src/Pages/Form/index.tsx
+++ frontend/src/Pages/Form/index.tsx
@@ -1,7 +1,45 @@
+import './style.css'
+
 function Form() {
-    return (
-        <h1>Pagina de formulario</h1>
-    )
+  const movie = {
+    id: 1,
+    image:
+      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    title: "The Witcher",
+    count: 2,
+    score: 4.5,
+  };
+
+  return (
+    <div className="reactmovie-form-container">
+      <img className="reactmovie-movie-card-image" src={movie.image} alt="The Witcher" />
+      <div className="reactmovie-card-bottom-container">
+        <h3>"The Witcher"</h3>
+        <form className="reactmovie-form">
+          <div className="form-group reactmovie-form-group">
+            <label htmlFor="email">Informe seu email</label>
+            <input type="email" className="form-control" id="email" />
+          </div>
+          <div className="form-group reactmovie-form-group">
+            <label htmlFor="score">Informe sua avaliação</label>
+            <select className="form-control" id="score">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+          <div className="reactmovie-form-btn-container">
+            <button type="submit" className="btn btn-primary reactmovie-btn">
+              Salvar
+            </button>
+          </div>
+        </form>
+        <button className="btn btn-primary reactmovie-btn mt-3">Cancelar</button>
+      </div>
+    </div>
+  );
 }
 
 export default Form;
