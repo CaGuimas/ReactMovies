d1a5cb92e7a4cf7f0d2212baa319beaa48410c65
Caio <jilsorem@gmail.com>
2022-03-10 03:50:03 -0300

    domain

---
+++ backend/src/main/java/com/mauxeroso/reactmovie/entities/Score.java
@@ -0,0 +1,55 @@
+package com.mauxeroso.reactmovie.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Entity
+@Table(name= "tb_score")
+public class Score {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	@Autowired
+	private ScorePK id;
+	
+	private Double value;
+	
+	public Score() {
+		
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+	
+	
+	
+
+}
